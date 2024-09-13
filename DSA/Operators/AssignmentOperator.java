db7269378e5dbf3a95ae4f161529f0f6a7809ac0
RAMESHYADAVBCA <ry275886@gmail.com>
2024-09-13 09:16:13 +0530

    Add a new file

---
+++ DSA/Operators/AssignmentOperator.java
@@ -0,0 +1,29 @@
+public class AssignmentOperator{
+    public static void main(String[] args){
+        // Assignment : =
+        // int A = 10;
+        // int B = 5;
+        // A = B;
+
+       // Assignment +=  
+        int A = 10;
+        A += 10; //A = A + 10;
+        System.out.println(A);
+
+        //Assignment: -=        
+        int B = 10;
+        B -= 10; // B = B - 10;
+        System.out.println(B);
+        
+        //Assignment: *=
+        int a = 20;
+        a *= 20; // a = a * 20;
+        System.out.println(a);
+
+        //Assignment: /=
+        int b = 10;
+        b /= 10; // b = b / 10;
+        System.out.println(b);
+      
+    }
+}
