9be71c7d5fe5aeb9ff0fe63d724d9c62f85e6351
RAMESHYADAVBCA <ry275886@gmail.com>
2024-09-16 07:34:50 +0530

    conditional Statement code example

---
+++ DSA/Conditional-Statement/Fever.java
@@ -0,0 +1,10 @@
+public class Fever{
+    public static void main(String[] args){
+        double temp = 103.5;
+        if(temp > 100){
+            System.out.println("you have fever");
+        } else{
+            System.out.println("you don't have a fever");
+        }
+    }
+}
