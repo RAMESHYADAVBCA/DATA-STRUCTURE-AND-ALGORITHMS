69d8e8aa50e80c29f8170c06e68521731a751e79
RAMESHYADAVBCA <ry275886@gmail.com>
2024-09-16 21:55:14 +0530

    Initial Addition of code

---
+++ DSA/Loops/Name.java
@@ -0,0 +1,11 @@
+public class Name{
+    public static void main(String[] args){
+        int counter = 0;
+
+        while(counter < 5){
+            System.out.println("Hello World");
+            counter++;
+        }
+        System.out.println("Printed HW 10x");
+    }
+}
