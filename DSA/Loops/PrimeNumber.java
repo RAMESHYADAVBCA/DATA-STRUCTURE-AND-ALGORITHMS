ea86455b63d5cd380dbde246ba0c0f807d459f99
RAMESHYADAVBCA <ry275886@gmail.com>
2024-09-19 13:37:02 +0530

    initial Addition code

--- DSA/Loops/PrimeNumber.java
+++ DSA/Loops/PrimeNumber.java
@@ -2,15 +2,20 @@ import java.util.*;
 public class PrimeNumber{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();        
-            if(n % 2 == 0){
-                System.out.println("prime number");
+        int n = sc.nextInt();
+        boolean isPrime = true;
+        for(int i = 2; i<=n-1; i++){
+            if(n % i == 0){
+                isPrime = false;
+            }
+        }
+                if(isPrime == true){
+                    System.out.println("n is prime");
                 }
-                 else{
-                    System.out.println(" not prime number");
-                }
-            
-
-           
+                    else {
+                        System.out.println(" n is not prime");
+                        }
     }
+
+
 }
